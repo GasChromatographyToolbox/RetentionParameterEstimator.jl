2f4e709cf576b8882f8ac8fae700b2bd317798bb
Jan Leppert <68964801+JanLeppert@users.noreply.github.com>
2022-10-14 10:04:13 +0200

    Update Load.jl

--- src/Load.jl
+++ src/Load.jl
@@ -3,13 +3,13 @@
 function load_chromatograms(file)
     n = open(f->countlines(f), file)
     col_df = DataFrame(CSV.File(file, header=1, limit=1))
-    column = Dict(   :L => col_test.L[1],
-                        :d => col_test.d[1],
-                        :df => col_test.df[1],
-                        :gas => col_test.gas[1],
-                        :pout => col_test.pout[1],
-                        :sp => col_test.sp[1],
-                        :time_unit => col_test.time_unit[1]
+    column = Dict(   :L => col_df.L[1],
+                        :d => col_df.d[1],
+                        :df => col_df.df[1],
+                        :gas => col_df.gas[1],
+                        :pout => col_df.pout[1],
+                        :sp => col_df.sp[1],
+                        :time_unit => col_df.time_unit[1]
                     )
     n_meas = Int((n - 2 - 3)/3) 
     TP = DataFrame(CSV.File(file, header=3, limit=n_meas))

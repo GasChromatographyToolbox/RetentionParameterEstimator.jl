a7a4dcd4fc8f82312d5d550ec6d8fcd31aa50fe8
Jan Leppert <68964801+JanLeppert@users.noreply.github.com>
2022-10-14 09:58:39 +0200

    Update Load.jl

--- src/Load.jl
+++ src/Load.jl
@@ -2,11 +2,20 @@
 
 function load_chromatograms(file)
     n = open(f->countlines(f), file)
-    column = DataFrame(CSV.File(file, header=1, limit=1)) # better in dictonary?
+    col_df = DataFrame(CSV.File(file, header=1, limit=1))
+    column = Dict(   :L => col_test.L[1],
+                        :d => col_test.d[1],
+                        :df => col_test.df[1],
+                        :gas => col_test.gas[1],
+                        :pout => col_test.pout[1],
+                        :sp => col_test.sp[1],
+                        :time_unit => col_test.time_unit[1]
+                    )
     n_meas = Int((n - 2 - 3)/3) 
     TP = DataFrame(CSV.File(file, header=3, limit=n_meas))
     PP = DataFrame(CSV.File(file, header=3+n_meas+1, limit=n_meas))
-    tRs = DataFrame(CSV.File(file, header=n-n_meas)) # depending on column.time_unit[1] calculate in s
+    tRs = DataFrame(CSV.File(file, header=n-n_meas))
     solute_names = names(tRs)[2:end] # filter non-solute names out (columnx)
+    filter!(x -> !occursin.("Column", x), solute_names)
     return column, TP, PP, tRs, solute_names
 end

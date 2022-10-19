93adc95c4f46b0efab54ccb80f84a6a4cb1d5046
Jan Leppert <68964801+JanLeppert@users.noreply.github.com>
2022-10-19 11:47:30 +0200

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
-using Test, RetentionParameterEstimator
+using Test, RetentionParameterEstimator, GasChromatographySimulator
 
 L = 30.0
 d = 0.25e-3
@@ -7,7 +7,7 @@ sp = "SLB5ms"
 gas = "He"
 TP = [40.0, 3.0, 15.0, 340.0, 5.0]
 PP = [150000.0, 3.0, 5000.0, 250000.0, 5.0]
-solutes = ["Decane", "2-Octanol"]
+solutes = ["Decane", "2-Octanol", "Pentadecane"]
 db_path = "./data"
 db_file = "Database_SLB5ms.csv"
 pout = "vacuum"
@@ -21,3 +21,31 @@ tR_sim, tR_sim_randn, par_sim = RetentionParameterEstimator.sim_test_chrom(L, d,
 @test tR_sim[1,1] == tR_sim[2,1]
 # more meaningful test?
 
+# compare GasChromatographySimulator.simulate() and RetentionParameterEstimator.tR_calc() 
+
+pl, sol = GasChromatographySimulator.simulate(par)
+
+Tchar = par.sub[1].Tchar
+θchar = par.sub[1].θchar
+ΔCp = par.sub[1].ΔCp
+opt = par.opt
+
+tR = RetentionParameterEstimator.tR_calc(Tchar, θchar, ΔCp, L, d, prog, opt, gas)
+
+pl.tR[1] == tR # false, because of odesys=true for GasChromatographySimulator
+    # and in RetentionParameterEstimator only the migration ODE is used 
+
+opt_ = GasChromatographySimulator.Options(ng=true)
+par_ = GasChromatographySimulator.Parameters(par.col, par.prog, par.sub, opt_)
+pl_, sol_ = GasChromatographySimulator.simulate(par_)
+
+opt__ = GasChromatographySimulator.Options(ng=true, odesys=false)
+par__ = GasChromatographySimulator.Parameters(par.col, par.prog, par.sub, opt__)
+pl__, sol__ = GasChromatographySimulator.simulate(par__)
+
+opt___ = GasChromatographySimulator.Options(ng=false, odesys=false)
+par___ = GasChromatographySimulator.Parameters(par.col, par.prog, par.sub, opt___)
+pl___, sol___ = GasChromatographySimulator.simulate(par___)
+
+[pl.tR[1], pl_.tR[1], pl__.tR[1], pl___.tR[1]].-tR
+# lowest difference for opt__ and opt___ to tR ≈ 1e-5

3d58ffa38cf699d6e76e153c37d22a0047b27df9
Jan Leppert <68964801+JanLeppert@users.noreply.github.com>
2022-10-23 12:25:35 +0200

    Update Loss.jl

--- src/Loss.jl
+++ src/Loss.jl
@@ -7,20 +7,10 @@ Calculates the retention time tR for a solute with the K-centric parameters `Tch
     
 """
 function tR_calc(Tchar, θchar, ΔCp, L, d, prog, opt, gas)
-	# df has no influence on the result (is hidden in Tchar, θchar, ΔCp)
-	R = 8.3145
-	k(x,t,Tchar,θchar,ΔCp) = exp((ΔCp/R + Tchar/θchar)*(Tchar/prog.T_itp(x,t)-1) + ΔCp/R*log(prog.T_itp(x,t)/Tchar))
-	#k(x,t,Tchar,θchar,ΔCp,d,df) = GasChromatographySimulator.retention_factor(x,t, prog.T_itp, d, df, Tchar, θchar, ΔCp, df/d)
-	rM(x,t,L,d) = GasChromatographySimulator.mobile_phase_residency(x,t, prog.T_itp, prog.Fpin_itp, prog.pout_itp, L, d, gas; ng=opt.ng, vis=opt.vis, control=opt.control)
-	r(t,p,x) = (1+k(x,t,p[1],p[2],p[3]))*rM(x,t,p[4],p[5])
-	#r(t,p,x) = (1+k(x,t,p[1],p[2],p[3],p[5],p[6]))*rM(x,t,p[4],p[5]) -> df has to be added as variable
-
-	# alternivly
-	#r(t,p,x) = residency(x, t, prog.T_itp, prog.Fpin_itp, prog.pout_itp, p[4], p[5], p[6], gas, p[1], p[2], p[3], p[6]/p[5]; ng=opt.ng, vis=opt.vis, control=opt.control)
+	r(t,p,x) = GasChromatographySimulator.residency(x, t, prog.T_itp, prog.Fpin_itp, prog.pout_itp, p[4], p[5], p[5]*1-e3, gas, p[1], p[2], p[3], 1e-3; ng=opt.ng, vis=opt.vis, control=opt.control)
 	t₀ = 0.0
 	xspan = (0.0, L)
 	p = [Tchar, θchar, ΔCp, L, d]
-	#p = [Tchar, θchar, ΔCp, L, d, df]
 	prob = ODEProblem(r, t₀, xspan, p)
 	solution = solve(prob, alg=opt.alg, abstol=opt.abstol, reltol=opt.reltol)
 	tR = solution.u[end]
@@ -40,19 +30,8 @@ function tR_calc(A, B, C, L, d, df, prog, opt, gas)
 	return tR
 end
 
-function tR_calc_(Tchar, θchar, ΔCp, L, d, df, prog, opt, gas)
-	r(t,p,x) = GasChromatographySimulator.residency(x, t, prog.T_itp, prog.Fpin_itp, prog.pout_itp, p[4], p[5], p[6], gas, p[1], p[2], p[3], p[6]/p[5]; ng=opt.ng, vis=opt.vis, control=opt.control)
-	t₀ = 0.0
-	xspan = (0.0, L)
-	p = [Tchar, θchar, ΔCp, L, d, df]
-	prob = ODEProblem(r, t₀, xspan, p)
-	solution = solve(prob, alg=opt.alg, abstol=opt.abstol, reltol=opt.reltol)
-	tR = solution.u[end]
-	return tR
-end
-
 """
-    loss(tR, Tchar, θchar, ΔCp, L, d, prog, opt, gas)
+    loss(tR, Tchar, θchar, ΔCp, L, d, prog, opt, gas; metric="quadratic")
 
 Loss function as sum of squares of the residuals between the measured and calculated retention times.
     
@@ -72,15 +51,7 @@ The output is a tuple of the following quantites:
 * `sum((tR.-tRcalc).^2)` ... sum of the squared residuals over m GC-programs and n solutes.
 * `tRcalc` ... mxn-array of the calculated retention times
 """
-function loss(tR, Tchar, θchar, ΔCp, L, d, prog, opt, gas)
-	# loss function as sum over all programs and solutes, fixed L and d
-	#gas = "He"
-	#tR = p[1]
-	#L = p[2]
-	#d = p[3]
-	#prog = p[4]
-	#opt = p[5]
-
+function loss(tR, Tchar, θchar, ΔCp, L, d, prog, opt, gas; metric="quadratic")
 	if length(size(tR)) == 1
 		ns = 1 # number of solutes
 		np = size(tR)[1] # number of programs
@@ -91,19 +62,23 @@ function loss(tR, Tchar, θchar, ΔCp, L, d, prog, opt, gas)
 		ns = size(tR)[2]
 		np = size(tR)[1]
 	end
-	#Tchar = p_Kcentric[1:ns] # Array length = number solutes
-	#θchar = p_Kcentric[ns+1:2*ns] # Array length = number solutes
-	#ΔCp = p_Kcentric[2*ns+1:3*ns] # Array length = number solutes
 	tRcalc = Array{Any}(undef, np, ns)
 	for j=1:ns
 		for i=1:np
 			tRcalc[i,j] = tR_calc(Tchar[j], θchar[j], ΔCp[j], L, d, prog[i], opt, gas)
 		end
 	end
-	return sum((tR.-tRcalc).^2), tRcalc
-end	
+	if metric == "abs"
+		l = sum(abs.(tR.-tRcalc))/(ns*np)
+	elseif metric == "quadratic"
+		l = sum((tR.-tRcalc).^2)/(ns*np)
+	else
+		l = sum((tR.-tRcalc).^2)/(ns*np)
+	end
+	return l, tRcalc
+end
 
-function loss(tR, A, B, C, L, d, df, prog, opt, gas)
+function loss(tR, A, B, C, L, d, df, prog, opt, gas; metric="quadratic")
 	# loss function as sum over all programs and solutes
 	if length(size(tR)) == 1
 		ns = 1 # number of solutes
@@ -121,25 +96,13 @@ function loss(tR, A, B, C, L, d, df, prog, opt, gas)
 			tRcalc[i,j] = tR_calc(A[j], B[j], C[j], L, d, df, prog[i], opt, gas)
 		end
 	end
-	return sum((tR.-tRcalc).^2), tRcalc
+	if metric == "abs"
+		l = sum(abs.(tR.-tRcalc))/(ns*np)
+	elseif metric == "quadratic"
+		l = sum((tR.-tRcalc).^2)/(ns*np)
+	else
+		l = sum((tR.-tRcalc).^2)/(ns*np)
+	end
+	return l, tRcalc
 end	
 
-function loss_(tR, Tchar, θchar, ΔCp, L, d, df, prog, opt, gas)
-	if length(size(tR)) == 1
-		ns = 1 # number of solutes
-		np = size(tR)[1] # number of programs
-	elseif length(size(tR)) == 0
-		ns = 1
-		np = 1
-	else
-		ns = size(tR)[2]
-		np = size(tR)[1]
-	end
-	tRcalc = Array{Any}(undef, np, ns)
-	for j=1:ns
-		for i=1:np
-			tRcalc[i,j] = tR_calc_(Tchar[j], θchar[j], ΔCp[j], L, d, df, prog[i], opt, gas)
-		end
-	end
-	return sum((tR.-tRcalc).^2), tRcalc
-end	

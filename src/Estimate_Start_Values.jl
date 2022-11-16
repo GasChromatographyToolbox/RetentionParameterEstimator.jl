63567dab35fdf5597e86ebcc17be9ac26327d46d
Jan Leppert <68964801+JanLeppert@users.noreply.github.com>
2022-11-16 14:12:32 +0100

    Update Estimate_Start_Values.jl

--- src/Estimate_Start_Values.jl
+++ src/Estimate_Start_Values.jl
@@ -90,9 +90,9 @@ function estimate_start_parameter_mean_elu_temp(tRs::DataFrame, col, prog; time_
 end
 
 function estimate_start_parameter(tR_meas::DataFrame, col, prog; time_unit="min", control="Pressure")
-    if length(prog) == 4
+    try
         Tchar_est, θchar_est, ΔCp_est, Telu_max = estimate_start_parameter_single_ramp(tR_meas, col, prog; time_unit=time_unit, control=control)
-    else 
+    catch 
         Tchar_est, θchar_est, ΔCp_est, Telu_max = estimate_start_parameter_mean_elu_temp(tR_meas, col, prog; time_unit=time_unit)
     end
     return Tchar_est, θchar_est, ΔCp_est, Telu_max

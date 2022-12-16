6e87a39b312ed752c46fee92dae6cb5b9422a007
Jan Leppert <68964801+JanLeppert@users.noreply.github.com>
2022-12-16 14:08:51 +0100

    Revert "Update Estimate_Start_Values.jl"
    This reverts commit 22c121b7b0d080920b540bf7a2abcf51012f23b6.

--- src/Estimate_Start_Values.jl
+++ src/Estimate_Start_Values.jl
@@ -64,7 +64,7 @@ function estimate_start_parameter_single_ramp(tRs::DataFrame, col, prog; time_un
         Telu_max[i] = maximum(Telu_meas[:,i])
         Tchar_est[i] = interp(0.0)
         θchar_est[i] = 22.0*(Tchar_est[i]/Tst)^0.7*(1000*col.df/col.d)^0.09
-        ΔCp_est[i] = -180.0 + 0.63*Tchar_est[i] # parameters from linear fit of all (Tchar, ΔCp) pairs in the database (RetentionData, 16.12.2022)
+        ΔCp_est[i] = 100.0
     end
     return Tchar_est, θchar_est, ΔCp_est, Telu_max
 end
@@ -81,7 +81,6 @@ function estimate_start_parameter_mean_elu_temp(tRs::DataFrame, col, prog; time_
 	Telu_max = Array{Float64}(undef, ns)
 	Tchar_elu = Array{Float64}(undef, ns)
 	θchar_elu = Array{Float64}(undef, ns)
-    ΔCp_est = Array{Float64}(undef, ns)
 	for j=1:ns
 		Telu = Array{Float64}(undef, nt)
 		for i=1:nt
@@ -90,8 +89,8 @@ function estimate_start_parameter_mean_elu_temp(tRs::DataFrame, col, prog; time_
 		Telu_max[j] = maximum(Telu)
 		Tchar_elu[j] = mean(Telu)
 		θchar_elu[j] = 22.0*(Tchar_elu[j]/273.15)^0.7*(1000*col.df/col.d)^0.09
-        ΔCp_est[i] = -180.0 + 0.63*Tchar_est[i] # parameters from linear fit of all (Tchar, ΔCp) pairs in the database (RetentionData, 16.12.2022)
 	end
+	ΔCp_elu = 100.0.*ones(ns)
 	return Tchar_elu, θchar_elu, ΔCp_elu, Telu_max
 end
 
